93c4661fb10a0d75ced9cbcbca67aa106bc6698e
Kilmar22 <72371533+Kilmar22@users.noreply.github.com>
2021-04-18 03:58:09 -0600

    Se agrega variable para indicar con que vista se va trabajar

--- resources/views/livewire/student-component.blade.php
+++ resources/views/livewire/student-component.blade.php
@@ -1,4 +1,10 @@
 <div class="row">
+    <div class="col-sm-3">
+        <!--Se pasará una variable para indicar con que vista se va a trabajar
+            esta variable se va a registrar en el componente-->
+        @include("livewire.$view")
+    </div>
+
     <div class="col-sm-9">
         @include('livewire.table')
     </div>
